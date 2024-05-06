7764010cfcaa2ae9840e93271bce5278aa2e1b75
Suraj Kumar <surajkumarok20@gmail.com>
2024-05-06 18:10:29 +0530

    using for and while loops

--- naturalNumber.cpp
+++ naturalNumber.cpp
@@ -11,6 +11,13 @@ int main()
     for(int i = 1; i<=number ; i++){
         cout<<i<<endl;
     }
+cout<<" ----------using while loo---------------"<<endl;
+// using while loop 
+int i = 1;
+while(i<=number){
+    cout<<i<<endl;
+    i++;
+}
 
     return 0;
 }
--- sumNaturalnumber.cpp
+++ sumNaturalnumber.cpp
@@ -11,6 +11,16 @@ int main()
     for(int i = 1; i<=number ; i++){
       sum = sum+i;
     }
-      cout<<"sum of first "<<number<<"number is: "<<sum<<endl;
+      cout<<"sum of first "<<number<<" number is: "<<sum<<endl;
+
+
+      cout<<" ----------using while loo---------------"<<endl;
+// using while loop 
+     int i = 1, sum2 = 0;
+    while(i<=number){
+    sum2 = sum2+i;
+    i++;
+    }
+     cout<<"sum of first "<<number<<" number is: "<<sum2<<endl;
     return 0;
 }
